5d38c1ac3eb0c261fbdccfc2d6d28f6deddbd7f4
ccaglaa <126245553+ccaglaa@users.noreply.github.com>
2023-06-12 19:33:41 +0200

    Add files via upload

---
+++ utils/save_load.py
@@ -0,0 +1,11 @@
+import tkinter
+import tkinter.filedialog
+
+def prompt_file():
+    """Create a Tk file dialog and cleanup when finished"""
+    top = tkinter.Tk()
+    top.withdraw()  # hide window
+    file_name = tkinter.filedialog.askopenfilename(parent=top)
+    top.destroy()
+    return file_name
+
